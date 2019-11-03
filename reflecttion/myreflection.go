621444b34d4fe78b08a9abd0859c8eafddb05951
Michael <13432849094@163.com>
2019-11-03 11:56:34 +0800

    补充反射知识点

--- reflecttion/myreflection.go
+++ reflecttion/myreflection.go
@@ -16,12 +16,18 @@ type User struct {
 	Age  int
 }
 
-func (u User) Hello() {
-	fmt.Println("hello world")
+// 嵌套类型如何反射
+type Manager struct {
+	User
+	title string
 }
 
-func Info(o interface{}) {
+func (u User) Hello(name string) {
+	fmt.Println("hello world ", name)
+}
 
+// 获取字段与方法
+func Info(o interface{}) {
 	t := reflect.TypeOf(o)
 	fmt.Println("type: ", t.Name())
 
@@ -41,8 +47,63 @@ func Info(o interface{}) {
 
 }
 
+// 基本类型通过反射改变值
+func BaseType() {
+	x := 123
+	v := reflect.ValueOf(&x)
+	v.Elem().SetInt(999)
+	fmt.Println(x)
+}
+
+// 修改自定义类型的数据
+func CustomType(o interface{}) {
+
+	v := reflect.ValueOf(o)
+
+	if v.Kind() == reflect.Ptr && !v.Elem().CanSet() {
+		fmt.Println("xxx")
+		return
+	} else {
+		v = v.Elem()
+	}
+
+	f := v.FieldByName("Name")
+	if !f.IsValid() {
+		fmt.Println("BAO")
+		return
+	}
+
+	if f.Kind() == reflect.String {
+		f.SetString("BYEBYE")
+	}
+}
+
+// 通过反射调用方法
+func CallMethod() {
+
+	user := User{1, "OK", 12}
+
+	value := reflect.ValueOf(user)
+	mv := value.MethodByName("Hello")
+
+	args := []reflect.Value{reflect.ValueOf("joe")}
+	mv.Call(args)
+}
+
 func main() {
 
-	u := User{1, "OK", 12}
-	Info(u)
+	//m := Manager{User: User{1, "OK", 12}, title: "title"}
+	//t := reflect.TypeOf(m)
+	//fmt.Println(t.FieldByIndex([]int{0, 0}))
+	//BaseType()
+
+	//u := User{1, "OK", 12}
+	//CustomType(&u)
+	//fmt.Println(u)
+
+	//u := User{1, "OK", 12}
+	//Info(u)
+
+	CallMethod()
+
 }
