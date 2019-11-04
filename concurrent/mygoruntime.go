238b859fb2ddfc25e545c778c1de31a50cb55fd8
Michael <13432849094@163.com>
2019-11-04 14:31:07 +0800

    通道

--- concurrent/mygoruntime.go
+++ concurrent/mygoruntime.go
@@ -7,10 +7,22 @@ import (
 	"time"
 )
 
-func main() {
+// 通过异步来阻塞主线程
+func Syn() {
+	group := sync.WaitGroup{}
+	group.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Println("GO2-", i)
+			group.Done()
+		}()
+	}
+	group.Wait()
+}
 
+// 通道缓存, 功能类似于 java 的 CountDownLatch
+func Channel() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	// 通道缓存, 功能类似于 java 的 CountDownLatch
 	//c := make(chan bool, 1)
 	c := make(chan bool, 10)
 	for i := 0; i < 10; i++ {
@@ -24,16 +36,12 @@ func main() {
 	for i := 0; i < 10; i++ {
 		<-c
 	}
+}
 
-	group := sync.WaitGroup{}
-	group.Add(10)
-	for i := 0; i < 10; i++ {
-		go func() {
-			fmt.Println("GO2-", i)
-			group.Done()
-		}()
-	}
-	group.Wait()
+func main() {
+
+	Channel()
+	Syn()
 
 	// TODO select 的使用（用于阻塞主线程）
 
